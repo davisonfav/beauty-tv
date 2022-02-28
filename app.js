e965486087d72d9288dd6921f33072ee8be77d1d
davison <davisonfav@gmail.com>
2022-02-28 17:35:04 -0500

    in progress

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
-setTimeout(f => {
-    document.getElementById('bg').style.backgroundImage = "url('images/spa_pes.png')"
+var config = {
+    "speed": 30,
+    "slides": [
+        {
+            "type": "image",
+            "bgImage": "avalie_google.png"
+        },
+        {
+            "type": "image",
+            "bgImage": "spa_pes.png"
+        },
+        {
+            "type": "image",
+            "bgImage": "alongamento_cilios.png"
+        }
+    ]
+}
+
+config.slides.forEach(function(slide, i) {
+    let elemDiv = document.createElement("div");
+    elemDiv.className = 'bg';
+    elemDiv.id = 'slide' + i;
+    elemDiv.style.backgroundImage = 'url("images/' + slide.bgImage + '")';
+    elemDiv.style.display = 'none';
+    document.body.appendChild(elemDiv);
+});
+
+let currentSlide = 0;
+document.getElementById('slide0').style.display = 'block';
+
+setInterval(f => {
+
+    let nextSlide = currentSlide + 1;
+    if (nextSlide >= config.slides.length)
+        nextSlide = 0;
+
+    document.getElementById('slide' + currentSlide).style.display = 'none';
+    document.getElementById('slide' + nextSlide).style.display = 'block';
+
+    currentSlide = nextSlide;
+    console.log(currentSlide);
+    console.log(nextSlide);
 }, 5000);
